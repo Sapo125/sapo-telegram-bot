8c72b98074f7f440d8c38a2fa84da0e208bab994
Sapo125 <69369585+Sapo125@users.noreply.github.com>
2022-03-24 15:27:51 +0100

    v1 - funzionante
    è possibile inviare foto, messaggio di testo o entrambi

--- Sapo Bot.py
+++ Sapo Bot.py
@@ -5,70 +5,75 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 import cv2
 import PySimpleGUI as sg
+#tengo il token, tanto cancellerò il bot
 token = "5104536132:AAH8vIQzth43uTkv3tBSW2-OeiGAoaBIsUY"
 
+#dichiarazione bot
 bot = telegram.Bot(token)
 
+#stampa info
 print (bot.get_me())
 
-chat_ids = set()
-
+#id della chat in cui deve agire (modificare in base alla chat da utilizzare)
 chatActive = "-1001298875748"
 
-
-
-sg.theme('DarkAmber')
-#file_types=estensioni
+#elenco estensioni supportate per l'invio immagini
 estensioni = [("JPEG (*.jpg)", "*.jpg"),
               ("PNG (*.png)", "*.png"),
               ("All files (*.*)", "*.*")]
-
+#layout GUI           
+sg.theme('DarkTeal7')
 layout = [  [sg.Text('Inserisci un messaggio')],
             [sg.Multiline(no_scrollbar=True, size=(50,6), key="testo")],
-            [sg.Button('invia testo'), sg.FileBrowse('invia foto',file_types=estensioni,key="image")] ]
+            [sg.Button('Invia messaggio'), sg.FileBrowse("Aggiungi foto",file_types=estensioni, key = "img", target="path"), sg.Button("Rimuovi foto")],
+            [sg.Text('',key = "check"), sg.Input('', key="path", visible=False)] ]
 
-def start(update, context):
-    chat_id = update.effective_chat.id
-    update.message.reply_text("Attivato")
-    print("start called from chat with id = {}".format(chat_id))
-    chat_ids.add(chat_id)
-
-def end(update, context):
-    chat_id = update.effective_chat.id
-    chat_ids.remove(chat_id)
-
-def message(msg):
-    try:
-        bot.send_message(chat_id=chatActive, text=msg)
-    except:
-        sg.Popup("errore nell'invio")
+#funzione per inviare il messaggio
+def message(txt,img):
+    if img == "":
+        try:
+            bot.send_message(chat_id=chatActive, text=txt)
+        except:
+            sg.Popup("errore nell'invio del testo, controlla e riprova\n(il messaggio potrebbe essere vuoto)")
+    else:
+        try:
+            #lettura immagine in byte
+            imgbyte = open(img, 'rb')
+            bot.send_photo(chat_id=chatActive, photo=imgbyte, caption=txt)
+        except:
+            sg.Popup("errore nell'invio del messaggio, controlla e riprova")
         
-#def img(msg):
-    
-    
-
+#main
 def main():
+    window = sg.Window('TEST', layout)
+
+    while True:
+        event, values = window.read(100)
+        #visualizzazione percorso foto (se presente)
+        if values["path"] != '':
+            window["check"].update("Foto inserita: {}".format(values["path"]))
+        else:
+            window["check"].update("")
+
+        #selezione funzione
+        if event == sg.WIN_CLOSED:
+            break
+        elif event == "Invia messaggio":
+            message(values["testo"],values["path"])
+        elif event == "Rimuovi foto":
+            window["path"].update("")
+
+    window.close()
+
+main()        
     
-    #LETTORE COMANDI
+    #LETTORE COMANDI E ESEMPIO DI COMANDI (li tengo che non si sa mai)
     #updater = Updater(token)
     #updater.dispatcher.add_handler(CommandHandler("start", start))
     #updater.dispatcher.add_handler(CommandHandler("msg", message))
     #updater.start_polling()
     #updater.idle()
-
-    window = sg.Window('TEST', layout)
-    while True:
-        event, values = window.read(100)
-        if event == sg.WIN_CLOSED:
-            break
-        elif event == "invia testo":
-            message(values["testo"])
-        elif event == "invia foto":
-            img(values["image"])
-        
-        
-    window.close()
-
-main()
-
-
+    
+    #def start(update, context):
+    #    chat_id = update.effective_chat.id
+    #    update.message.reply_text("Attivato")
